65e77102b76b4ada39eafb5e3c39f0191a73e425
Ryan Sandor Richards <sandor.richards@gmail.com>
2020-01-09 22:06:16 -0500

    Production build of the simple flash cards.

---
+++ ghpages/precache-manifest.05a443a4f5bb9fc2e631e01ae102a670.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e3faaaafaa61a6a18eed603bb148bc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94f073c0901dae419dc0",
+    "url": "/static/css/main.7690f4e4.chunk.css"
+  },
+  {
+    "revision": "aa0d3586ff310725c2dd",
+    "url": "/static/js/2.5e2f3714.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.5e2f3714.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94f073c0901dae419dc0",
+    "url": "/static/js/main.5072d560.chunk.js"
+  },
+  {
+    "revision": "419871d527fd42161101",
+    "url": "/static/js/runtime-main.eb587304.js"
+  }
+]);
