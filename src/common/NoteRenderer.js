344a65c8627b9e1122555aa975fbc93a135a49c8
Ryan Sandor Richards <sandor.richards@gmail.com>
2020-01-10 19:08:58 -0500

    Basic rendering support for the different staffs.

--- src/common/NoteRenderer.js
+++ src/common/NoteRenderer.js
@@ -1,38 +1,99 @@
-import Vex from 'vexflow'
+import store from '../store'
+import { Flow } from 'vexflow'
 
 export function renderNote (note, clef) {
-  const VF = Vex.Flow
+  const {settings} = store.getState()
+  const {bass, treble} = settings.clef
+  if (bass && treble) return renderGrandStaffNote(note, clef)
+  if (bass) return renderBassStaffNote(note, clef)
+  if (treble) return renderTrebleStaffNote(note, clef)
+  return ''
+}
+
+function renderBassStaffNote (note, clef) {
   const div = document.createElement('div')
-  const renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+  const renderer = new Flow.Renderer(div, Flow.Renderer.Backends.SVG)
 
   const context = renderer.getContext()
   renderer.resize(325, 380)
   context.scale(1.75, 1.75)
 
-  const treble = new VF.Stave(20, 0, 160)
-  treble.addClef("treble")
-  treble.setContext(context).draw()
-
-  const bass = new VF.Stave(20, 100, 160)
+  const bass = new Flow.Stave(0, 40, 185)
   bass.addClef("bass")
   bass.setContext(context).draw()
 
-  const connector = new VF.StaveConnector(treble, bass)
+  const notes = [
+    new Flow.StaveNote({ clef: 'bass', keys: [note], duration: "w" })
+  ]
+
+  const voice = new Flow.Voice({ num_beats: 4, beat_value: 4 })
+  voice.addTickables(notes)
+
+  const formatter = new Flow.Formatter()
+  formatter.joinVoices([voice]).format([voice], 150)
+  voice.draw(context, bass)
+
+  return svgToDataUrl(div.querySelector('svg'))
+}
+
+function renderTrebleStaffNote (note, clef) {
+  const div = document.createElement('div')
+  const renderer = new Flow.Renderer(div, Flow.Renderer.Backends.SVG)
+
+  const context = renderer.getContext()
+  renderer.resize(325, 380)
+  context.scale(1.75, 1.75)
+
+  const treble = new Flow.Stave(0, 40, 185)
+  treble.addClef("treble")
+  treble.setContext(context).draw()
+
+  const notes = [
+    new Flow.StaveNote({ clef: 'treble', keys: [note], duration: "w" })
+  ]
+
+  const voice = new Flow.Voice({ num_beats: 4, beat_value: 4 })
+  voice.addTickables(notes)
+
+  const formatter = new Flow.Formatter()
+  formatter.joinVoices([voice]).format([voice], 150)
+  voice.draw(context, treble)
+
+  return svgToDataUrl(div.querySelector('svg'))
+}
+
+function renderGrandStaffNote (note, clef) {
+  const div = document.createElement('div')
+  const renderer = new Flow.Renderer(div, Flow.Renderer.Backends.SVG)
+
+  const context = renderer.getContext()
+  renderer.resize(325, 380)
+  context.scale(1.75, 1.75)
+
+  const treble = new Flow.Stave(20, 0, 160)
+  treble.addClef("treble")
+  treble.setContext(context).draw()
+
+  const bass = new Flow.Stave(20, 100, 160)
+  bass.addClef("bass")
+  bass.setContext(context).draw()
+
+  const connector = new Flow.StaveConnector(treble, bass)
   connector.setType('singleLeft')
   connector.setContext(context).draw()
 
-  const brace = new VF.StaveConnector(treble, bass)
+  const brace = new Flow.StaveConnector(treble, bass)
   brace.setType('brace')
   brace.setContext(context).draw()
 
   const notes = [
-    new VF.StaveNote({ clef, keys: [note], duration: "w" })
+    new Flow.StaveNote({ clef, keys: [note], duration: "w" })
   ]
 
-  const voice = new VF.Voice({ num_beats: 4, beat_value: 4 })
+  const voice = new Flow.Voice({ num_beats: 4, beat_value: 4 })
   voice.addTickables(notes)
 
-  const formatter = new VF.Formatter()
+  const formatter = new Flow.Formatter()
   formatter.joinVoices([voice]).format([voice], 150)
 
   voice.draw(context, clef === 'treble' ? treble : bass)
