673890e75b8c9caa7a2a767cd90db9ccede94620
Ryan Sandor Richards <sandor.richards@gmail.com>
2020-01-11 13:23:03 -0500

    Added context support for treble clef renderer.

--- src/common/NoteRenderer.js
+++ src/common/NoteRenderer.js
@@ -12,86 +12,23 @@ export function renderNote (clef, note, settings) {
   }
 }
 
-function svgToDataUrl (svg) {
-  return `data:image/svg+xml;base64,${window.btoa(new XMLSerializer().serializeToString(svg))}`
-}
-
-function getContext (settings) {
-  const div = document.createElement('div')
-  const renderer = new Flow.Renderer(div, Flow.Renderer.Backends.SVG)
-  const context = renderer.getContext()
-  renderer.resize(325, 380)
-  context.scale(1.75, 1.75)
-
-  if (settings.theme === 'dark') {
-    context.setFillStyle('#efefe9')
-    context.setStrokeStyle('#efefe9')
-  }
-
-  return { context, div }
-}
-
 function renderBassStaffNote (note, settings) {
-  const { theme } = settings
   const { context, div } = getContext(settings)
-
+  const notes = getNotes(note, 'bass', settings)
   const bass = new Flow.Stave(0, 40, 185)
   bass.addClef("bass")
   bass.setContext(context).draw()
-
-  let notes = []
-
-  if (settings.context === 'whole') {
-    notes = [ new Flow.StaveNote({ clef: 'bass', keys: [note], duration: "w" })]
-  } else if (settings.context === 'random-rests') {
-    const durations = randomBarDurations()
-    const noteIndex = randomInt(durations.length)
-    notes = durations.map((duration, index) => {
-      if (index === noteIndex) {
-        const stem_direction = note.split('/')[1] >= 3 ? -1 : 1
-        return new Flow.StaveNote({ clef: 'bass', keys: [note], duration, stem_direction })
-      }
-      return new Flow.StaveNote({ clef: 'bass', keys: ['d/3'], duration: duration + 'r' })
-    })
-  } else {
-    const durations = randomBarDurations()
-    const noteIndex = randomInt(durations.length)
-    notes = durations.map((duration, index) => {
-      if (index === noteIndex) {
-        const stem_direction = note.split('/')[1] >= 3 ? -1 : 1
-        const n = new Flow.StaveNote({ clef: 'bass', keys: [note], duration, stem_direction })
-        if (theme === 'light') {
-          n.setStyle({ strokeStyle: '#169', fillStyle: '#169' })
-        } else if (theme === 'dark') {
-          n.setStyle({ strokeStyle: '#49d', fillStyle: '#49d' })
-        }
-        return n
-      } else {
-        const randomNote = NoteGenerator.uniformRandomNote('bass')
-        const stem_direction = randomNote.split('/')[1] >= 3 ? -1 : 1
-        return new Flow.StaveNote({ clef: 'bass', keys: [randomNote], duration, stem_direction })
-      }
-    })
-  }
-
   Flow.Formatter.FormatAndDraw(context, bass, notes, { auto_beam: true })
   return svgToDataUrl(div.querySelector('svg'))
 }
 
 function renderTrebleStaffNote (note, settings) {
   const { context, div } = getContext(settings)
-
+  const notes = getNotes(note, 'treble', settings)
   const treble = new Flow.Stave(0, 40, 185)
-  treble.addClef("treble")
+  treble.addClef('treble')
   treble.setContext(context).draw()
-
-  const notes = [new Flow.StaveNote({ clef: 'treble', keys: [note], duration: "w" })]
-  const voice = new Flow.Voice({ num_beats: 4, beat_value: 4 })
-  voice.addTickables(notes)
-  const formatter = new Flow.Formatter()
-  formatter.joinVoices([voice]).format([voice], 150)
-  voice.draw(context, treble)
-
+  Flow.Formatter.FormatAndDraw(context, treble, notes, { auto_beam: true })
   return svgToDataUrl(div.querySelector('svg'))
 }
 
@@ -122,4 +59,63 @@ function renderGrandStaffNote (note, clef, settings) {
   voice.draw(context, clef === 'treble' ? treble : bass)
 
   return svgToDataUrl(div.querySelector('svg'))
+}
+function svgToDataUrl (svg) {
+  return `data:image/svg+xml;base64,${window.btoa(new XMLSerializer().serializeToString(svg))}`
+}
+
+function getContext (settings) {
+  const div = document.createElement('div')
+  const renderer = new Flow.Renderer(div, Flow.Renderer.Backends.SVG)
+  const context = renderer.getContext()
+  renderer.resize(325, 380)
+  context.scale(1.75, 1.75)
+
+  if (settings.theme === 'dark') {
+    context.setFillStyle('#efefe9')
+    context.setStrokeStyle('#efefe9')
+  }
+
+  return { context, div }
+}
+
+function getNotes (note, clef, settings) {
+  const { theme } = settings
+  let notes = []
+  const restNote = clef === 'bass' ? 'd/3' : 'b/4'
+
+  if (settings.context === 'whole') {
+    notes = [ new Flow.StaveNote({ clef, keys: [note], duration: "w" })]
+  } else if (settings.context === 'random-rests') {
+    const durations = randomBarDurations()
+    const noteIndex = randomInt(durations.length)
+    notes = durations.map((duration, index) => {
+      if (index === noteIndex) {
+        const stem_direction = note.split('/')[1] >= 3 ? -1 : 1
+        return new Flow.StaveNote({ clef, keys: [note], duration, stem_direction })
+      }
+      return new Flow.StaveNote({ clef, keys: [restNote], duration: duration + 'r' })
+    })
+  } else {
+    const durations = randomBarDurations()
+    const noteIndex = randomInt(durations.length)
+    notes = durations.map((duration, index) => {
+      if (index === noteIndex) {
+        const stem_direction = note.split('/')[1] >= 3 ? -1 : 1
+        const n = new Flow.StaveNote({ clef, keys: [note], duration, stem_direction })
+        if (theme === 'light') {
+          n.setStyle({ strokeStyle: '#169', fillStyle: '#169' })
+        } else if (theme === 'dark') {
+          n.setStyle({ strokeStyle: '#49d', fillStyle: '#49d' })
+        }
+        return n
+      } else {
+        const randomNote = NoteGenerator.uniformRandomNote(clef)
+        const stem_direction = randomNote.split('/')[1] >= 3 ? -1 : 1
+        return new Flow.StaveNote({ clef, keys: [randomNote], duration, stem_direction })
+      }
+    })
+  }
+
+  return notes
 }
